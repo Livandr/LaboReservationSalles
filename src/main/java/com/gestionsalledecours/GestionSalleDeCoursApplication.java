4d01c1a38eec46bd10b5b8ff72f291139e981498
Liva Andr <livandr@gmail.com>
2023-02-16 16:56:34 +0100

    Initial commit

---
+++ src/main/java/com/gestionsalledecours/GestionSalleDeCoursApplication.java
@@ -0,0 +1,13 @@
+package com.gestionsalledecours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSalleDeCoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSalleDeCoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionsalledecours/GestionSalleDeCoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionsalledecours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionSalleDeCoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
