7704b46f2dd5c5fd996282fc2000b8e20ff73445
Liva Andr <livandr@gmail.com>
2023-02-21 01:16:30 +0100

    up

--- src/main/java/com/gestionsalledecours/models/forms/ConnectionForm.java
+++ src/main/java/com/gestionsalledecours/models/forms/ConnectionForm.java
@@ -1,16 +1,14 @@
 package com.gestionsalledecours.models.forms;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+
 public class ConnectionForm {
     @NotBlank(message = "enter your login")
     private String login;
 
     @NotBlank(message = "enter your password")
-    @Pattern(regexp = "^^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{4,}$",
-            message = "password must contain atleast 1 uppercase, 1 lowercase, 1 special character and 1 digit ")
     private String password;
 }
--- src/main/java/com/gestionsalledecours/validation/constraints/PasswordConfirmed.java
+++ src/main/java/com/gestionsalledecours/validation/constraints/PasswordConfirmed.java
@@ -1,6 +1,6 @@
 package com.gestionsalledecours.validation.constraints;
 
-import com.gestionsalledecours.validation.validators.EmailNotTakenValidator;
+import com.gestionsalledecours.validation.validators.PasswordConfirmedValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
@@ -9,12 +9,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.FIELD)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = EmailNotTakenValidator.class)
-public @interface EmailNotTaken {
+@Constraint(validatedBy = PasswordConfirmedValidator.class)
+public @interface PasswordConfirmed {
 
-    String message() default "email already taken";
+    String message() default "wrong password";
 
     Class<?>[] groups() default { };
 
